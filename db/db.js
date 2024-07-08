4d01215b9802efa0aa48b7346f26a23fa20f144c
Gabriel Ivan Carrillo <gabrielcarrillo943@gmail.com>
2024-07-08 00:52:05 -0300

    add pool

--- db/db.js
+++ db/db.js
@@ -1,13 +1,20 @@
 const mysql = require("mysql2");
 
-const connection = mysql.createConnection({
+const pool = mysql.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
-    database: process.env.DB_NAME
-});
+    database: process.env.DB_name,
+    waitForConnections: true,
+    connectionLimit: 10,
+    maxIdle: 10,
+    idleTimeout: 60000,
+    queueLimit: 0,
+    enableKeepAlive: true,
+    keepAliveInitialDelay: 0,
+  });
 
-connection.connect((error) => {
+pool.connect((error) => {
   if (error) {
     console.error(error);
   } else{
@@ -15,4 +22,4 @@ connection.connect((error) => {
   }
 });
 
-module.exports = connection;
+module.exports = pool;
