33c01c51bfd25474d08b0dfecfc225cf75f78676
Gabriel Ivan Carrillo <gabrielcarrillo943@gmail.com>
2024-07-08 00:11:04 -0300

    cors

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 require("dotenv").config()
 const express = require("express")
 const app = express()
+const cors = require('cors')
 const PORT = process.env.PORT || 3001
 
 app.use(express.json())
+app.use(cors())
 
 app.use('/carrito', require('./routes/carrito.router'));
 
